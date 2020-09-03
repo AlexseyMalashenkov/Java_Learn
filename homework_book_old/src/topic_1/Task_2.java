92be1f7633fa0329c5195f270e28cf1de3e9e54a
Aleksey Malashenkov <alexseymalashenkov@gmail.com>
2020-09-03 18:34:14 +0300

    Delete

--- homework_book_old/src/topic_1/Task_2.java
+++
@@ -1,9 +0,0 @@
-package topic_1;
-
-public class Task_2 {
-    public static void main(String[] args) {
-        int b = 4;
-        int c = 13;
-        System.out.println(4 * (b + c - 1) / 2);
-    }
-}
