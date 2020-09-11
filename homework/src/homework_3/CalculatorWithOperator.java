b5b649a700e5eb40a4479647a0652bfba0f1aca2
Aleksey Malashenkov <alexseymalashenkov@gmail.com>
2020-09-11 12:39:28 +0300

    I added second task from homework #3

--- homework/src/homework_3/CalculatorWithOperator.java
+++ homework/src/homework_3/CalculatorWithOperator.java
@@ -2,32 +2,16 @@ package homework_3;
 
 public class CalculatorWithOperator {
 
-    public int addNum(int firstNum, int secondNum) {
-        return firstNum + secondNum;
-    }
-
     public double addNum(double firstNum, double secondNum) {
         return firstNum + secondNum;
-    }
-
-    public int subNum(int firstNum, int secondNum) {
-        return firstNum - secondNum;
     }
 
     public double subNum(double firstNum, double secondNum) {
         return firstNum - secondNum;
     }
 
-    public int multiNum(int firstNum, int secondNum) {
-        return firstNum * secondNum;
-    }
-
     public double multiNum(double firstNum, double secondNum) {
         return firstNum * secondNum;
-    }
-
-    public int divNum(int firstNum, int secondNum) {
-        return firstNum / secondNum;
     }
 
     public double divNum(double firstNum, double secondNum) {
@@ -48,14 +32,6 @@ public class CalculatorWithOperator {
         }
     }
 
-    public int absNum(int num) {
-        if (num >= 0) {
-            return num;
-        } else {
-            return num + (-num * 2);
-        }
-    }
-
     public double absNum(double num) {
         if (num >= 0) {
             return num;
@@ -65,16 +41,6 @@ public class CalculatorWithOperator {
     }
 
     //Метод Ньютона-Рафсона
-
-    public double sqrtNum(int num) {
-        double tmp;
-        double sqrtRoot = num / 2;
-        do {
-            tmp = sqrtRoot;
-            sqrtRoot = (tmp + (num / tmp)) / 2;
-        } while ((tmp - sqrtRoot) != 0);
-        return sqrtRoot;
-    }
 
     public double sqrtNum(double num) {
         double tmp;
--- homework/src/homework_3/CalculatorWithOperatorMain.java
+++ homework/src/homework_3/CalculatorWithOperatorMain.java
@@ -3,7 +3,11 @@ package homework_3;
 public class CalculatorWithOperatorMain {
     public static void main(String[] args) {
         CalculatorWithOperator calc = new CalculatorWithOperator();
-        System.out.println(calc.absNum(-7655));
-        System.out.println(calc.sqrtNum(100.4));
+
+        double result = calc.addNum(calc.addNum(4.1, calc.multiNum(15, 7)), calc.expNum(calc.divNum(28, 5), 2));
+        System.out.printf("4.1 + 15 * 7 + (28 / 5)^ 2 = " + "%.2f", result);
+        System.out.println();
+        System.out.println(calc.divNum(result, 0));    // В обоих случаях у меня деление на 0 даёт бесконечность т.к. метод принимает double.
+        System.out.println(calc.divNum(result, 0.0d)); //Если был бы int - выдало бы исключение.
     }
 }
