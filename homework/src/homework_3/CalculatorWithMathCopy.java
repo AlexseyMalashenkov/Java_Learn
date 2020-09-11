e6550c09798d9b428febc0cd5275ea6320a7ab67
Aleksey Malashenkov <alexseymalashenkov@gmail.com>
2020-09-11 12:57:59 +0300

    I added task #4 from homework #3

--- homework/src/homework_3/CalculatorWithMathCopy.java
+++ homework/src/homework_3/CalculatorWithMathCopy.java
@@ -19,36 +19,14 @@ public class CalculatorWithMathCopy {
     }
 
     public double expNum(double num, int exp) {
-        double result = num;
-
-        if (num > 0) {
-            for (int i = exp; i > 1; i--) {
-                result *= num;
-            }
-            return result;
-        } else {
-            System.out.println("Вы ввели не положительное число");
-            return 0;
-        }
+        return Math.pow(num, exp);
     }
 
     public double absNum(double num) {
-        if (num >= 0) {
-            return num;
-        } else {
-            return num * -1;
-        }
+        return Math.abs(num);
     }
 
-    //Метод Ньютона-Рафсона
-
     public double sqrtNum(double num) {
-        double tmp;
-        double squareRoot = num / 2;
-        do {
-            tmp = squareRoot;
-            squareRoot = (tmp + (num / tmp)) / 2;
-        } while ((tmp - squareRoot) != 0);
-        return squareRoot;
+        return Math.sqrt(num);
     }
 }
--- homework/src/homework_3/CalculatorWithOperatorMain.java
+++ homework/src/homework_3/CalculatorWithOperatorMain.java
@@ -2,13 +2,14 @@ package homework_3;
 
 public class CalculatorWithOperatorMain {
     public static void main(String[] args) {
-        CalculatorWithOperator calc = new CalculatorWithOperator();
+        CalculatorWithOperator calcWithOperator = new CalculatorWithOperator();
 
-        double result = calc.addNum(calc.addNum(4.1, calc.multiNum(15, 7)), calc.expNum(calc.divNum(28, 5), 2));
-        System.out.printf("4.1 + 15 * 7 + (28 / 5)^ 2 = " + "%.2f", result);
+        double resultWithOperator = calcWithOperator.addNum(calcWithOperator.addNum(4.1, calcWithOperator.multiNum(15, 7)), calcWithOperator.expNum(calcWithOperator.divNum(28, 5), 2));
+        System.out.printf("4.1 + 15 * 7 + (28 / 5)^ 2 = " + "%.2f", resultWithOperator);
         System.out.println();
-        System.out.println(calc.divNum(result, 0));    // В обоих случаях у меня деление на 0 даёт бесконечность т.к. метод принимает double.
-        System.out.println(calc.divNum(result, 0.0d)); //Если был бы int - выдало бы исключение.
-        CalculatorWithMathExtends calculator = new CalculatorWithMathExtends();
+        System.out.println(calcWithOperator.divNum(resultWithOperator, 0));    // В обоих случаях у меня деление на 0 даёт бесконечность т.к. метод принимает double.
+        System.out.println(calcWithOperator.divNum(resultWithOperator, 0.0d)); //Если был бы int - выдало бы исключение.
+
+        CalculatorWithMathExtends calcWithMathExtends = new CalculatorWithMathExtends();
     }
 }
