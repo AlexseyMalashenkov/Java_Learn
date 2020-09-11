1ab67f0885878a1b71dd6c1ed90cf650740b3dfe
Aleksey Malashenkov <alexseymalashenkov@gmail.com>
2020-09-11 13:10:14 +0300

    I added task #6,7 from homework #3

--- homework/src/homework_3/CalculatorMain.java
+++ homework/src/homework_3/CalculatorMain.java
@@ -1,12 +1,12 @@
 package homework_3;
 
-public class CalculatorWithOperatorMain {
+public class CalculatorMain {
     public static void main(String[] args) {
-        
+
         CalculatorWithOperator calcWithOperator = new CalculatorWithOperator();
 
         double resultWithOperator = calcWithOperator.addNum(calcWithOperator.addNum(4.1, calcWithOperator.multiNum(15, 7)), calcWithOperator.expNum(calcWithOperator.divNum(28, 5), 2));
-        System.out.printf("4.1 + 15 * 7 + (28 / 5)^ 2 = " + "%.2f", resultWithOperator);
+        System.out.printf("Задание №2) 4.1 + 15 * 7 + (28 / 5)^ 2 = " + "%.2f", resultWithOperator);
         System.out.println();
         System.out.println(calcWithOperator.divNum(resultWithOperator, 0));    // В обоих случаях у меня деление на 0 даёт бесконечность т.к. метод принимает double.
         System.out.println(calcWithOperator.divNum(resultWithOperator, 0.0d));//Если был бы int - выдало бы исключение.
@@ -14,9 +14,17 @@ public class CalculatorWithOperatorMain {
         CalculatorWithMathCopy calcWithMathCopy = new CalculatorWithMathCopy();
 
         double resultWithMathCopy = calcWithMathCopy.addNum(calcWithMathCopy.addNum(4.1, calcWithMathCopy.multiNum(15, 7)), calcWithMathCopy.expNum(calcWithMathCopy.divNum(28, 5), 2));
-        System.out.printf("4.1 + 15 * 7 + (28 / 5)^ 2 = " + "%.2f", resultWithMathCopy);
+        System.out.printf("Задание №5) 4.1 + 15 * 7 + (28 / 5)^ 2 = " + "%.2f", resultWithMathCopy);
         System.out.println();
         System.out.println(calcWithMathCopy.divNum(resultWithMathCopy, 0));    // В обоих случаях у меня деление на 0 даёт бесконечность т.к. метод принимает double.
         System.out.println(calcWithMathCopy.divNum(resultWithMathCopy, 0.0d));//Если был бы int - выдало бы исключение.
+
+        CalculatorWithMathExtends calcWithMathExtends = new CalculatorWithMathExtends();
+
+        double resultWithMathExtends = calcWithMathExtends.addNum(calcWithMathExtends.addNum(4.1, calcWithMathExtends.multiNum(15, 7)), calcWithMathExtends.expNum(calcWithMathExtends.divNum(28, 5), 2));
+        System.out.printf("Задание №7) 4.1 + 15 * 7 + (28 / 5)^ 2 = " + "%.2f", resultWithMathExtends);
+        System.out.println();
+        System.out.println(calcWithMathExtends.divNum(resultWithMathExtends, 0));    // В обоих случаях у меня деление на 0 даёт бесконечность т.к. метод принимает double.
+        System.out.println(calcWithMathExtends.divNum(resultWithMathExtends, 0.0d));//Если был бы int - выдало бы исключение.
     }
 }
--- homework/src/homework_3/CalculatorWithMathExtends.java
+++ homework/src/homework_3/CalculatorWithMathExtends.java
@@ -1,5 +1,38 @@
 package homework_3;
 
 public class CalculatorWithMathExtends extends CalculatorWithOperator {
+    @Override
+    public double addNum(double firstNum, double secondNum) {
+        return super.addNum(firstNum, secondNum);
+    }
 
+    @Override
+    public double subNum(double firstNum, double secondNum) {
+        return super.subNum(firstNum, secondNum);
+    }
+
+    @Override
+    public double multiNum(double firstNum, double secondNum) {
+        return super.multiNum(firstNum, secondNum);
+    }
+
+    @Override
+    public double divNum(double firstNum, double secondNum) {
+        return super.divNum(firstNum, secondNum);
+    }
+
+    @Override
+    public double expNum(double num, int exp) {
+        return Math.pow(num, exp);
+    }
+
+    @Override
+    public double absNum(double num) {
+        return Math.abs(num);
+    }
+
+    @Override
+    public double sqrtNum(double num) {
+        return Math.sqrt(num);
+    }
 }
