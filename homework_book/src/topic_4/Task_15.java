9e215c2ada31eec8189f611a450853baa261dec1
Aleksey Malashenkov <alexseymalashenkov@gmail.com>
2020-09-18 09:39:22 +0300

    I added task #15 from homebook which now counts all primes

--- homework_book/src/topic_4/Task_15.java
+++ homework_book/src/topic_4/Task_15.java
@@ -1,20 +1,52 @@
 package topic_4;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Task_15 {
     public static void main(String[] args) {
-        findSecondPrimeNumber(50, 70);
+        int count = 4;
+        int n = 1_000_000_000;
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(5);
+        numbers.add(7);
+
+        Date startTime = new Date();
+
+        for (int i = 11; i <= n; i+=2) {
+            if (simple(i)) {
+                count++;
+                numbers.add(i);
+            }
+        }
+        Date finishTime = new Date();
+        long ct = finishTime.getTime() - startTime.getTime();
+        System.out.println("Количество простых чисел: " + count);
+        System.out.print(numbers);
+        System.out.println();
+        System.out.println("Время вычислений: " + ct + "ms");
     }
 
-    public static void findSecondPrimeNumber(int first, int second) {
-        int tmp = 0;
-        for (int i = first; i < second; i++) {
-            if(i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0) {
-                tmp++;
+    static boolean simple(int a) {
+        int p = 0;
+        if ((a % 2 == 0)||(a % 10 == 5)) {
+            return false;
+        }
+        else {
+            for (int j = 3; j <= Math.sqrt(a); j += 2) {
+                if (a % j == 0) {
+                    p += 1;
+                    break;
+                }
             }
-            if (tmp == 2) {
-                System.out.println(i);
-                break;
-            }
+        }
+        if (p > 0) {
+            return false;
+        } else {
+            return true;
         }
     }
 }
