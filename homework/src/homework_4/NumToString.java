54a2621afff8a809209ac2bd6d7f1263bb40fad0
Aleksey Malashenkov <alexseymalashenkov@gmail.com>
2020-09-24 14:30:54 +0300

    I added task #1 from homework #4 about string

---
+++ homework/src/homework_4/NumToString.java
@@ -0,0 +1,98 @@
+package homework_4;
+
+public class NumToString {
+    private final int num;
+
+    private final String[] BELLOW_TWENTY = { "ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" };
+    private final String[] TENS = { "", "", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто" };
+    private final String[] HUNDREDS = { "", "сто", "двести" , "тристо", "четыресто", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот" };
+    private final String[] THOUSANDS = { "тысяча", "тысячи", "тысяч" };
+    private final String[] MILLIONS = { "миллион", "миллиона", "миллионов" };
+
+    NumToString(int num) {
+        this.num = num;
+    }
+
+    public String intToStr() {
+        String result;
+
+        if (this.num >= -999_999_999 && this.num <= 999_999_999) {
+            result = this.num < 0 ? "минус " + numToStr(this.num) : "" + numToStr(this.num);
+        } else {
+            result = "введите другое число";
+        }
+
+        return result;
+    }
+
+    private String numToStr(int num) {
+        int tmp = Math.abs(num);
+
+        if (tmp < 20) {
+            return this.BELLOW_TWENTY[tmp];
+        }
+        if (tmp < 100) {
+            return this.TENS[tmp / 10] + ((tmp % 10 != 0) ? " " + this.BELLOW_TWENTY[tmp % 10]: "");
+        }
+        if (tmp < 1000) {
+            return this.HUNDREDS[tmp / 100] + ((tmp % 100 != 0) ? " " + numToStr(tmp % 100): "");
+        }
+        if (tmp < 2000) {
+            return this.THOUSANDS[0] + ((tmp % 1000 != 0) ? " " + numToStr(tmp % 1000) : "");
+        }
+        if (tmp < 3000) {
+            return "две " + this.THOUSANDS[1] + ((tmp % 1000 != 0) ? " " + numToStr(tmp % 1000): "");
+        }
+        if (tmp < 5000) {
+            return this.BELLOW_TWENTY[tmp / 1000] + " " + this.THOUSANDS[1] + ((tmp % 1000 != 0) ? " " + numToStr(tmp % 1000): "");
+        }
+        if (tmp < 1_000_000) {
+            return specialValueHandlingThousands(findFirstNum(tmp)) + ((tmp % 1000 != 0) ? " " + numToStr(tmp % 1000): "");
+        }
+        if (tmp < 2_000_000) {
+            return this.MILLIONS[0] + ((tmp % 1_000_000 != 0) ? " "  + numToStr(tmp % 1_000_000): "");
+        }
+        if (tmp < 5_000_000) {
+            return this.BELLOW_TWENTY[tmp / 1_000_000] + " " + this.MILLIONS[1] + ((tmp % 1_000_000 != 0) ? " " + numToStr(tmp % 1_000_000): "");
+        }
+        return specialValueHandlingMillions(findFirstNum(tmp)) + ((tmp % 1_000_000 != 0) ? " " + numToStr(tmp % 1_000_000): "");
+    }
+
+    private int findFirstNum(int num) {
+        if (num < 1_000_000) {
+            return num / 1000;
+        }
+        if (num < 1_000_000_000){
+            return num / 1_000_000;
+        }
+
+        return num;
+    }
+
+    private String specialValueHandlingThousands(int num) {
+        if (num % 10 == 1 && num != 11 && num != 1 && num != 111) {
+            return numToStr(num - (num % 10)) + " одна " + this.THOUSANDS[0];
+        }
+        if (num % 10 == 2 && num != 12 && num != 2 && num != 112) {
+            return numToStr(num - (num % 10)) + " две " + this.THOUSANDS[1];
+        }
+        if ((num % 10 == 3 && num != 13 && num !=3 && num != 113) || (num % 10 == 4 && num != 14 && num !=4 && num != 114)) {
+            return numToStr(num - (num % 10)) + " " + this.BELLOW_TWENTY[num % 10] + " " + this.THOUSANDS[1];
+        }
+        else {
+            return numToStr(num) + " " + this.THOUSANDS[2];
+        }
+    }
+
+    private String specialValueHandlingMillions(int num) {
+        if (num % 10 == 1 && num != 11 && num != 1 && num != 111) {
+            return numToStr(num - (num % 10)) + " " + this.BELLOW_TWENTY[num % 10] + " " + this.MILLIONS[0];
+        }
+        if ((num % 10 == 2 && num != 12 && num != 2 && num != 112) || (num % 10 == 3 && num != 13 && num !=3  && num != 113) || (num % 10 == 4 && num != 14 && num !=4 && num != 114)) {
+            return numToStr(num - (num % 10)) + " " + this.BELLOW_TWENTY[num % 10] + " " + this.MILLIONS[1];
+        }
+        else {
+            return numToStr(num) + " " + this.MILLIONS[2];
+        }
+    }
+}
--- homework/src/homework_4/StringsMain.java
+++ homework/src/homework_4/StringsMain.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
 
 public class StringsMain {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число от -999_999_999 до 999_999_999");
-        int num = scanner.nextInt();
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Введите число от -999_999_999 до 999_999_999");
+        //int num = scanner.nextInt();
+        //
+        //NumToString numInt = new NumToString(num);
+        //System.out.println(numInt.intToStr());
 
-        NumsToString numsInt = new NumsToString(num);
-        numsInt.intToStr();
+        int[] array = new int[1000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) Math.round((Math.random() * 2_000_000_000) - 1_000_000_000);
+            NumToString numInt = new NumToString(array[i]);
+            System.out.println(array[i] + " \t" + numInt.intToStr());
+        }
     }
 }
