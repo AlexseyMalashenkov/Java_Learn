4371a6b1d2282f01a615c91156464879762a99f7
Aleksey Malashenkov <alexseymalashenkov@gmail.com>
2020-09-25 11:54:44 +0300

    fix: Some gramma bugs. add: Some gramma bugs...

--- homework/src/homework_4/WorkWithString.java
+++ homework/src/homework_4/WorkWithString.java
@@ -4,16 +4,19 @@ import java.util.Locale;
 
 public class WorkWithString {
     private final String[] BELLOW_TWENTY = { "ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" };
+    private final String[] BELLOW_TWENTY_DOUBLE = { "ноль", "одна", "две", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" };
     private final String[] TENS = { "", "", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто" };
     private final String[] HUNDREDS = { "", "сто", "двести" , "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот" };
     private final String[] THOUSANDS = { "тысяча", "тысячи", "тысяч" };
     private final String[] MILLIONS = { "миллион", "миллиона", "миллионов" };
 
+    private boolean isDouble = false;
+
     public String intToStr(int numInt) {
         String result;
 
         if (numInt >= -999_999_999 && numInt <= 999_999_999) {
-            result = numInt < 0 ? "минус " + numToStr(numInt) : "" + numToStr(numInt);
+            result = numInt < 0 ? "минус " + numToStr(numInt, this.isDouble) : "" + numToStr(numInt, this.isDouble);
         } else {
             result = "введите другое число";
         }
@@ -26,8 +29,16 @@ public class WorkWithString {
         int numAfterComma = 2;
 
         if (numDouble >= -999_999_999.999 && numDouble <= 999_999_999.999) {
-            result = numDouble < 0 ? "минус " + numToStr((int)numDouble) + " целых " + numToStr(desCast(numDouble, numAfterComma)) + " сотых" :
-                                     "" + numToStr((int)numDouble) + " целых " + numToStr(desCast(numDouble, numAfterComma)) + " сотых" ;
+            String tmp;
+            if ((int)numDouble % 10 == 1) {
+                tmp = " целая ";
+            }
+            else {
+                tmp = " целых ";
+            }
+
+            result = numDouble < 0 ? "минус " + numToStr((int)numDouble, !this.isDouble) + tmp + numToStr(desCast(numDouble, numAfterComma), !this.isDouble) + " сотых" :
+                                     "" + numToStr((int)numDouble, !this.isDouble) + tmp + numToStr(desCast(numDouble, numAfterComma), !this.isDouble) + " сотых" ;
         } else {
             result = "введите другое число";
         }
@@ -47,37 +58,45 @@ public class WorkWithString {
         return result;
     }
 
-    private String numToStr(int num) {
+    private String numToStr(int num, boolean isDouble) {
         int tmp = Math.abs(num);
 
-        if (tmp < 20) {
-            return this.BELLOW_TWENTY[tmp];
+        if (isDouble) {
+            if (tmp < 20) {
+                return this.BELLOW_TWENTY_DOUBLE[tmp];
+            }
+        } else {
+            if (tmp < 20) {
+                return this.BELLOW_TWENTY[tmp];
+            }
         }
+
+
         if (tmp < 100) {
             return this.TENS[tmp / 10] + ((tmp % 10 != 0) ? " " + this.BELLOW_TWENTY[tmp % 10]: "");
         }
         if (tmp < 1000) {
-            return this.HUNDREDS[tmp / 100] + ((tmp % 100 != 0) ? " " + numToStr(tmp % 100): "");
+            return this.HUNDREDS[tmp / 100] + ((tmp % 100 != 0) ? " " + numToStr(tmp % 100, this.isDouble): "");
         }
         if (tmp < 2000) {
-            return this.THOUSANDS[0] + ((tmp % 1000 != 0) ? " " + numToStr(tmp % 1000) : "");
+            return this.THOUSANDS[0] + ((tmp % 1000 != 0) ? " " + numToStr(tmp % 1000, this.isDouble) : "");
         }
         if (tmp < 3000) {
-            return "две " + this.THOUSANDS[1] + ((tmp % 1000 != 0) ? " " + numToStr(tmp % 1000): "");
+            return "две " + this.THOUSANDS[1] + ((tmp % 1000 != 0) ? " " + numToStr(tmp % 1000, this.isDouble): "");
         }
         if (tmp < 5000) {
-            return this.BELLOW_TWENTY[tmp / 1000] + " " + this.THOUSANDS[1] + ((tmp % 1000 != 0) ? " " + numToStr(tmp % 1000): "");
+            return this.BELLOW_TWENTY[tmp / 1000] + " " + this.THOUSANDS[1] + ((tmp % 1000 != 0) ? " " + numToStr(tmp % 1000, this.isDouble): "");
         }
         if (tmp < 1_000_000) {
-            return specialValueHandlingThousands(findFirstNum(tmp)) + ((tmp % 1000 != 0) ? " " + numToStr(tmp % 1000): "");
+            return specialValueHandlingThousands(findFirstNum(tmp)) + ((tmp % 1000 != 0) ? " " + numToStr(tmp % 1000, this.isDouble): "");
         }
         if (tmp < 2_000_000) {
-            return this.MILLIONS[0] + ((tmp % 1_000_000 != 0) ? " "  + numToStr(tmp % 1_000_000): "");
+            return this.MILLIONS[0] + ((tmp % 1_000_000 != 0) ? " "  + numToStr(tmp % 1_000_000, this.isDouble): "");
         }
         if (tmp < 5_000_000) {
-            return this.BELLOW_TWENTY[tmp / 1_000_000] + " " + this.MILLIONS[1] + ((tmp % 1_000_000 != 0) ? " " + numToStr(tmp % 1_000_000): "");
+            return this.BELLOW_TWENTY[tmp / 1_000_000] + " " + this.MILLIONS[1] + ((tmp % 1_000_000 != 0) ? " " + numToStr(tmp % 1_000_000, this.isDouble): "");
         }
-        return specialValueHandlingMillions(findFirstNum(tmp)) + ((tmp % 1_000_000 != 0) ? " " + numToStr(tmp % 1_000_000): "");
+        return specialValueHandlingMillions(findFirstNum(tmp)) + ((tmp % 1_000_000 != 0) ? " " + numToStr(tmp % 1_000_000, this.isDouble): "");
     }
 
     private int findFirstNum(int num) {
@@ -98,16 +117,16 @@ public class WorkWithString {
         boolean isFour = num % 10 == 4 && num != 14 && num !=4 && num != 114;
 
         if (isOne) {
-            return numToStr(num - (num % 10)) + " одна " + this.THOUSANDS[0];
+            return numToStr(num - (num % 10), this.isDouble) + " одна " + this.THOUSANDS[0];
         }
         if (isTwo) {
-            return numToStr(num - (num % 10)) + " две " + this.THOUSANDS[1];
+            return numToStr(num - (num % 10), this.isDouble) + " две " + this.THOUSANDS[1];
         }
         if (isThree || isFour) {
-            return numToStr(num - (num % 10)) + " " + this.BELLOW_TWENTY[num % 10] + " " + this.THOUSANDS[1];
+            return numToStr(num - (num % 10), this.isDouble) + " " + this.BELLOW_TWENTY[num % 10] + " " + this.THOUSANDS[1];
         }
         else {
-            return numToStr(num) + " " + this.THOUSANDS[2];
+            return numToStr(num, this.isDouble) + " " + this.THOUSANDS[2];
         }
     }
 
@@ -118,13 +137,13 @@ public class WorkWithString {
         boolean isFour = num % 10 == 4 && num != 14 && num !=4 && num != 114;
 
         if (isOne) {
-            return numToStr(num - (num % 10)) + " " + this.BELLOW_TWENTY[num % 10] + " " + this.MILLIONS[0];
+            return numToStr(num - (num % 10), this.isDouble) + " " + this.BELLOW_TWENTY[num % 10] + " " + this.MILLIONS[0];
         }
         if (isTwo || isThree || isFour) {
-            return numToStr(num - (num % 10)) + " " + this.BELLOW_TWENTY[num % 10] + " " + this.MILLIONS[1];
+            return numToStr(num - (num % 10), this.isDouble) + " " + this.BELLOW_TWENTY[num % 10] + " " + this.MILLIONS[1];
         }
         else {
-            return numToStr(num) + " " + this.MILLIONS[2];
+            return numToStr(num, this.isDouble) + " " + this.MILLIONS[2];
         }
     }
 
@@ -136,13 +155,14 @@ public class WorkWithString {
     }
 
     private String rightWeek(int day) {
-        if (day % 10 == 2 && day % 10 == 3 && day % 10 == 4) {
-            return day / 7 + " недели";
+        int tmp = day / 7;
+        if (tmp % 10 == 2 || tmp % 10 == 3 || tmp % 10 == 4) {
+            return tmp + " недели";
         }
-        if (day % 10 == 1) {
-            return day / 7 + " неделя";
+        if (tmp % 10 == 1) {
+            return tmp + " неделя";
         } else {
-            return day / 7 + " недель";
+            return tmp + " недель";
         }
     }
 }
