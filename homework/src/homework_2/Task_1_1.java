565a76108756f1bd568ba8044c16af9f82bd12ff
Aleksey Malashenkov <alexseymalashenkov@gmail.com>
2020-10-08 12:19:31 +0300

    fix: corrected the homework #2 according to the comments

--- homework/src/homework_2/Task_1_1.java
+++ homework/src/homework_2/Task_1_1.java
@@ -3,10 +3,15 @@ package homework_2;
 public class Task_1_1 {
     public static void main(String[] args) {
         int result = 1;
+
         try {
-            for (int i = 1; i <= Integer.parseInt(args[0]); i++) {
+            if (args.length > 1) {
+                System.out.println("В программу можно передать только один аргумент");
+            }
+            int tmp = Integer.parseInt(args[0]);
+            for (int i = 1; i <= tmp; i++) {
                 result *= i;
-                if (i == Integer.parseInt(args[0])) {
+                if (i == tmp) {
                     System.out.print(i);
                 } else {
                     System.out.print(i + " * ");
@@ -15,7 +20,7 @@ public class Task_1_1 {
             System.out.println(" = " + result);
         }
         catch (Exception exception) {
-            System.out.println("Вы не ввели аргумент к исполняемой программе");
+            System.out.println("Вы не ввели аргумент к исполняемой программе или он не корректный");
         }
     }
 }
