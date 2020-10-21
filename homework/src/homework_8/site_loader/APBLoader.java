636e48865990e1e104997d6a61226910cddeb428
Aleksey Malashenkov <alexseymalashenkov@gmail.com>
2020-10-21 12:09:02 +0300

    think

---
+++ homework/src/homework_8/site_loader/APBLoader.java
@@ -0,0 +1,18 @@
+package homework_8.site_loader;
+
+import homework_8.utils.WorkWithXML;
+
+import java.util.Map;
+
+public class APBLoader extends SiteLoader{
+    @Override
+    public double load(Currency currencyName) {
+        return 0;
+    }
+
+    @Override
+    protected double handle(String content, Currency currencyName) {
+
+        return 0;
+    }
+}
